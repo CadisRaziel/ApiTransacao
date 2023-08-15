00b4311af2b74c22bf8859e82438ba21805d38c6
vitu <vitorcsharp@hotmail.com>
2023-08-15 17:15:27 -0300

    criando a classe transaction no nosso domain

---
+++ src/main/java/com/picpaysimplified/domain/transaction/Transaction.java
@@ -0,0 +1,37 @@
+package com.picpaysimplified.domain.transaction;
+
+import com.picpaysimplified.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+///transactions -> no plural pois tabelas NOSQL utiliza a palavra reservada 'transaction' no singular !!
+@Entity(name = "transactions")
+@Table(name = "transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amount;
+
+    ///referencia pra quem foi o pagador(sender) e o que recebeu(receiver)
+    @ManyToOne //-> um usuario pode ter varias transações(Many), mais uma transação só pode ter um usuario(One)
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne //-> um usuario pode ter varias transações(Many), mais uma transação só pode ter um usuario(One)
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+
+    private LocalDateTime timestamp;
+}
